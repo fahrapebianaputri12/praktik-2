3b0552580a8b6d73d9f304021555be423fdba860
Fahra Pebiana Putri <fahrapebianaputri2@gmail.com>
2025-12-14 21:45:13 +0700

    Initial commit for praktik-2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Ganti 'nama-repo' dengan NAMA REPOSITORY Anda di GitHub
-  base: "/nama-repo/", //
+  base: "/praktik-2-deployment/", //
   plugins: [react()],
 })
