b40be6550a3c115e791aae70f098b8194ea74575
Fahra Pebiana Putri <fahrapebianaputri2@gmail.com>
2025-12-14 20:14:02 +0700

    Deploy to Github Pages

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  // Ganti 'nama-repo' dengan NAMA REPOSITORY Anda di GitHub
-  base: "/nama-repo/", //
-  plugins: [react()],
-})
